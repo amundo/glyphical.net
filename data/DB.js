abea2354bc287c4cd05d41ddf5f86762fcad7ae4
Patrick Hall <pathall@gmail.com>
2023-07-24 12:38:27 -0400

    refactoring DB for use elsewhere

---
+++ data/DB.js
@@ -0,0 +1,48 @@
+
+class DB {
+  constructor(){
+    this.fetch()
+  }
+
+  async fetch(){
+    let url = new URL('./hieroglyphs.json', import.meta.url)
+    let hieroglyphsResponse = await fetch(url)
+    let {hieroglyphs, metadata} = await hieroglyphsResponse.json()
+
+    this.hieroglyphs = hieroglyphs
+    this.metadata = metadata
+  }
+
+  get categories(){
+    return this.hieroglyphs
+      .reduce((categories, hieroglyph) => {
+        if(!categories.includes(hieroglyph.category)){
+          categories.push(hieroglyph.category)
+        }
+        return categories
+      }, [])
+  }
+  get byCategory(){
+    if(!this.cachedByCategory){
+      this.cachedByCategory = this.hieroglyphs
+      .reduce((byCategory, hieroglyph) => {
+        if(!byCategory[hieroglyph.category]){
+          byCategory[hieroglyph.category] = []
+        }
+        byCategory[hieroglyph.category].push(hieroglyph)
+        return byCategory
+      }, {})
+    }
+    return this.cachedByCategory
+  }
+
+  search(query){
+    console.log('searching', query)
+    return this.hieroglyphs
+      .filter(hieroglyph => {
+        return match(query,hieroglyph)
+      })
+  }
+}
+
+export {DB}
