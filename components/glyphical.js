91ffdd385ce3ff7f357b563e84fb990caef7edc0
Patrick Hall <pathall@gmail.com>
2023-09-25 14:00:36 -0400

    update imports

--- components/glyphical.js
+++ components/glyphical.js
@@ -1,4 +1,8 @@
 export {SearchHieroglyphs} from "./search-hieroglyphs/SearchHieroglyphs.js"
 export {DataViewer} from 'https://docling.net/book/docling/components/data-viewer/DataViewer.js'
+export {NavBar} from './nav-bar/NavBar.js'
+export {GodCarousel} from '../data/gods/god-carousel/GodCarousel.js'
 // export {DataViewer} from 'http://localhost:1111/book/docling/components/data-viewer/DataViewer.js'
 
+
+console.log({SearchHieroglyphs,DataViewer,NavBar,GodCarousel})
