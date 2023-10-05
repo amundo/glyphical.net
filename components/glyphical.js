a785489fe644ce0ffe668b465aca2c6a28480cd8
Patrick Hall <pathall@gmail.com>
2023-10-05 11:53:23 -0400

    forgot to update index js

--- components/glyphical.js
+++ components/glyphical.js
@@ -2,5 +2,5 @@ export {SearchHieroglyphs} from "./search-hieroglyphs/SearchHieroglyphs.js"
 export {DataViewer} from 'https://docling.net/book/docling/components/data-viewer/DataViewer.js'
 export {NavBar} from './nav-bar/NavBar.js'
 export {GodCarousel} from '../data/gods/god-carousel/GodCarousel.js'
-
+export {QuizGame} from './quiz-game/QuizGame.js'
 
