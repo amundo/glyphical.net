e074b30f941204821c8796c9bb9303ce31d82b5b
Patrick Hall <pathall@gmail.com>
2023-10-24 13:30:05 -0400

    fix http link

--- components/glyphical.js
+++ components/glyphical.js
@@ -1,5 +1,5 @@
 export {SearchHieroglyphs} from "./search-hieroglyphs/SearchHieroglyphs.js"
-export {DataViewer} from 'http://pathall.net/data-viewer/v2.0.0/DataViewer.js'
+export {DataViewer} from 'https://pathall.net/data-viewer/v2.0.0/DataViewer.js'
 export {NavBar} from './nav-bar/NavBar.js'
 export {GodCarousel} from '../data/gods/god-carousel/GodCarousel.js'
 export {QuizGame} from './quiz-game/QuizGame.js'
