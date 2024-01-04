0c58d68d0894c7330a18b877c641e8dddf627377
Patrick Hall <pathall@gmail.com>
2024-01-04 15:47:35 -0500

    div.section to section

---
+++ coptic/lambdin/div-to-section.js
@@ -0,0 +1,30 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts"
+import { walk } from "https://deno.land/std/fs/mod.ts"
+
+function transformParagraphs(htmlString) {
+  const doc = new DOMParser().parseFromString(htmlString, "text/html");
+  if (!doc) throw new Error("Unable to parse the HTML string.");
+
+  // Find all paragraphs with class 'indented'
+  const sectionDivs = [...doc.querySelectorAll("div.class")];
+
+  sectionDivs.forEach((sectionDiv, i) => {
+    sectionDiv.outerHTML = `<section data-section-number="${i}">${sectionDiv.innerHTML}</section>`
+  })
+  
+  return doc.body.innerHTML
+}
+
+async function updateFiles() {
+  let files = []
+  for await (const entry of walk("lessons", { exts: [".html"] })) {
+    files.push(entry.name)
+    let htmlString = await Deno.readTextFile(entry.path)
+    let fixedHtmlString = await transformParagraphs(htmlString)
+    await Deno.writeTextFile(entry.path, fixedHtmlString)
+  }
+  return files
+}
+
+await updateFiles()
+
