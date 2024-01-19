fad4f30d193d94cd814d06e589bf6d46f01d9601
Patrick Hall <pathall@gmail.com>
2024-01-19 15:29:40 -0500

    Refactor table shuffle logic

--- coptic/games/table-shuffle/table-shuffle.js
+++ coptic/games/table-shuffle/table-shuffle.js
@@ -14,10 +14,33 @@ cells
     cell.originalRow = cell.parentElement
   })
 
-cells.sort(() => Math.random() < .5)
+
+
+
 rows.forEach((row) => row.cells[1].remove())
 
-let shuffledIndices = Array.from(Array(cells.length), (x, i) => i)
+let indices = Array.from(Array(cells.length), (x, i) => i)
+
+let shuffle = array => {
+  let currentIndex = array.length
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]]
+  }
+
+  return array;
+}
+
+let shuffledIndices = shuffle(indices)
 
 cells.forEach((cell, i) => {
   cell.setAttribute
