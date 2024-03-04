fe6e5343e7b903a179d90069b7f4b4059ef63d73
Patrick Hall <pathall@gmail.com>
2024-03-04 10:58:06 -0500

    copy header to footer

--- coptic/lambdin/copy-header-to-footer.js
+++ coptic/lambdin/copy-header-to-footer.js
@@ -7,15 +7,16 @@ import { updateFiles } from "./update-lesson-html-files.js"
 function copyHeaderIntoFooter(htmlString) {
   const doc = new DOMParser().parseFromString(htmlString, "text/html")
   if (!doc) throw new Error("Unable to parse the HTML string.")
-  let footer = doc.createElement('footer')
-  let main = doc.querySelector('main')
-  let header = doc.querySelector('header')
-  let headerClone = header.cloneNode(true)
-  main.append(footer)
 
-  if ( doc.querySelector("header.lesson-header") ){
-    footer.append(headerClone)
+  if(doc.querySelector('header#lesson-header')){
+    let footer = doc.createElement('footer')
+    let main = doc.querySelector('main')
+    let header = doc.querySelector('header')
+    footer.innerHTML = header.innerHTML
+    footer.id = 'lesson-footer'
+    main.after(footer)
   }
+
 
   return `<!doctype html>
   ${doc.documentElement.outerHTML}
